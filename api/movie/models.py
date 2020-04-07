843be5ece20f45329d26d3132d14d55dd4f7dcb6
Cage_Ps <cage_pas@users.noreply.github.com>
2020-04-07 10:02:37 +0700

    delete llist of the movie genre

--- api/movie/models.py
+++ api/movie/models.py
@@ -5,37 +5,6 @@ from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
-GENRE_CHOICES =(
-                ('absuwh', 'Absurdist/surreal/whimsical'),
-                ('action','Action'),
-                ('adventure', 'Adventure'),
-                ('anime', 'Animation'),
-                ('comedy', 'Comedy'),
-                ('crime', 'Crime'),
-                ('drama', 'Drama'),
-                ('fantasy', 'Fantasy'),
-                ('historical', 'Historical'),
-                ('historicalf', 'Historical fiction'),
-                ('horror', 'Horror'),
-                ('magical','Magical realism'),
-                ('mystery', 'Mystery'),
-                ('paranoid', 'Paranoid fiction'),
-                ('philosophical', 'Philosophical'),
-                ('political', 'Political'),
-                ('puppetry', 'Puppetry'),
-                ('romance', 'Romance'),
-                ('saga', 'Saga'),
-                ('staire', 'Satire'),
-                ('science', 'Science Fiction'),
-                ('social', 'Social'),
-                ('speculative', 'Speculative'),
-                ('thriller', 'Thriller'),
-                ('urban', 'Urban'),
-                ('western', 'Western'),
-)
 
 
 
@@ -48,7 +17,7 @@ class Movies(models.Model):
     # movie title
     title  = models.CharField(max_length=255, null=False, blank=False)
     # movie genre
-    genre = models.CharField(choices= GENRE_CHOICES, default='Action', max_length=100)
+    genre = models.CharField(null=False, blank=False, max_length=100)
     # name of the director(s)
     director = models.CharField(max_length=255, null=False, blank=False)
     # name of the producer(s)
@@ -64,7 +33,7 @@ class Movies(models.Model):
 
     class Meta:
         ordering = ('created', )
-        
+
 
 
     def __str__(self):
